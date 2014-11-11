ff37194d846faade57af15d8188c10151e55fd55
Tom Hood <networkerror@gmail.com>
2014-11-11 10:27:51 +0000

    fix(git): add pull default

--- sources/all/gitconfig.sh
+++ sources/all/gitconfig.sh
@@ -2,3 +2,4 @@ git config --global user.email "networkerror@gmail.com"
 git config --global user.name "Tom Hood"
 git config --global core.editor vim
 git config --global push.default current
+git config --global pull.default current
