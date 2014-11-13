ab95d03f1d4fd7c5d22c9b82db776b5f9e221029
Tom Hood <tom.hood@reachlocal.com>
2014-11-13 13:57:32 -0800

    fix(mac): fix aliases, install gitconfig once

--- sources/mac/aliases.sh
+++ sources/mac/aliases.sh
@@ -7,5 +7,5 @@ alias vim="mvim -v"
 
 alias client="title CLIENT && cd ~/Code/cpi/client && gulp serve"
 alias gateway="title GATEWAY && cd ~/Code/cpi/gateway && SKIP_AUTH=true be rails s --port 8001"
-alias gatewaystub="title GATEWAY && cd ~/Code/cpi/gateway && STUB_SERVICES=true SKIP_AUTH=true be rails
+alias gatewaystub="title GATEWAY && cd ~/Code/cpi/gateway && STUB_SERVICES=true SKIP_AUTH=true be rails"
 alias reporting="title REPORTING && cd ~/Code/cpi/reporting && be rails s --port 8002"
