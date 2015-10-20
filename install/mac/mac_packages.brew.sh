6680d435b8f8a24c018848b7baa7dd3fe866def9
Tom Hood <networkerror@gmail.com>
2015-10-20 10:09:37 -0700

    Add brew cask and SQL pro - MAC

--- install/mac/mac_packages.brew.sh
+++ install/mac/mac_packages.brew.sh
@@ -4,3 +4,7 @@ brew install vim nmap maven git git-extras gource wget ack
 
 # Dev stuff
 brew install autoconf automake cmake
+
+# GUI - app installer
+brew install caskroom/cask/brew-cask
+brew cask install sequel-pro
