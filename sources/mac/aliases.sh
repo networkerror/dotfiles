8935c0eb7476140d6f114b4e4f224f2876f8ebf7
Tom Hood <networkerror@gmail.com>
2015-10-20 09:38:18 -0700

    Removed ReachLocal-specific stuff from aliases

--- sources/mac/aliases.sh
+++ sources/mac/aliases.sh
@@ -2,10 +2,4 @@ function title {
   echo -ne "\033]0;"$*"\007"
 }
 
-alias vi="mvim -v"
-alias vim="mvim -v"
-
-alias client="title CLIENT && cd ~/Code/cpi/client && gulp serve"
-alias gateway="title GATEWAY && cd ~/Code/cpi/gateway && SKIP_AUTH=true be rails s --port 8001"
-alias gatewaystub="title GATEWAY && cd ~/Code/cpi/gateway && STUB_SERVICES=true SKIP_AUTH=true be rails"
-alias reporting="title REPORTING && cd ~/Code/cpi/reporting && be rails s --port 8002"
+# To set a title: alias gatewaystub="title GATEWAY && ..."
